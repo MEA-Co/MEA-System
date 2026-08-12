62f86ef797a6e7d41acfd57f3c6abd27c6840469
이동민 <ldm9100@mea.kr>
2026-08-12 13:02:16 +0900

    feat: lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,17 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypeScript from 'eslint-config-next/typescript';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
-    ],
-  },
+const eslintConfig = defineConfig([
+  ...nextCoreWebVitals,
+  ...nextTypeScript,
 
   {
     plugins: {
       'simple-import-sort': simpleImportSort,
     },
+
     rules: {
       'simple-import-sort/imports': [
         'error',
@@ -34,7 +19,7 @@ const eslintConfig = [
           groups: [
             [
               '^node:',
-              `^(assert|buffer|child_process|crypto|dns|events|fs|http|https|module|os|path|process|stream|timers|tty|url|util|zlib)(/|$)`,
+              '^(assert|buffer|child_process|crypto|dns|events|fs|http|https|module|os|path|process|stream|timers|tty|url|util|zlib)(/|$)',
             ],
 
             ['^@?\\w'],
@@ -42,40 +27,40 @@ const eslintConfig = [
             ['^@/'],
 
             ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+
             ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
 
             ['^.+\\.s?css$'],
           ],
         },
       ],
-      'simple-import-sort/exports': 'error',
-    },
-  },
 
-  {
-    rules: {
+      'simple-import-sort/exports': 'error',
+
       'no-console': 'warn',
+
       '@typescript-eslint/no-unused-vars': [
         'warn',
-
         {
           args: 'all',
-
           argsIgnorePattern: '^_',
-
           caughtErrors: 'all',
-
           caughtErrorsIgnorePattern: '^_',
-
           destructuredArrayIgnorePattern: '^_',
-
           varsIgnorePattern: '^_',
-
           ignoreRestSiblings: true,
         },
       ],
     },
   },
-];
+
+  globalIgnores([
+    'node_modules/**',
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
 
 export default eslintConfig;
